9789a88589ce5c53917e1c6e0c85060807cc44ee
Cesar Ortega <cortegafrias1@buffs.wtamu.edu>
2023-05-08 14:50:37 -0500

    Initial Commit.1

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIDM3312_FINALPROJECT.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
